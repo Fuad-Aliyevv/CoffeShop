a469bd97412931f45e8bee0d4e386731a578cff9
Fuad-hub-bit <fuad.aliyev.1993@gmail.com>
2026-04-08 19:58:43 +0400

    update footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import { NavLink } from "react-router-dom";
 import "./Footer.css";
 
 export default function Footer() {
@@ -8,22 +9,35 @@ export default function Footer() {
     <footer className="footer">
       <div className="container footer-content">
 
-      
+
         <div>
           <h6>{t("footer.aboutTitle")}</h6>
           <p>{t("footer.aboutText")}</p>
         </div>
 
+  
         <div>
           <h6>{t("footer.links")}</h6>
           <ul>
-            <li>{t("footer.home")}</li>
-            <li>{t("footer.menu")}</li>
-            <li>{t("footer.contact")}</li>
+            <li>
+              <NavLink to="/" className="footer-link">
+                {t("footer.home")}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/menu" className="footer-link">
+                {t("footer.menu")}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact" className="footer-link">
+                {t("footer.contact")}
+              </NavLink>
+            </li>
           </ul>
         </div>
 
-   
+
         <div>
           <h6>{t("footer.contactTitle")}</h6>
           <p>{t("footer.address")}</p>
